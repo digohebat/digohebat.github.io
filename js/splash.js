4147a4fd210427a5524c1c4f604d93aef9ede1d5
Wima P <61861426+digohebat@users.noreply.github.com>
2022-02-01 18:59:59 +0700

    Added Splash Screen
    Grow With Wima Splash Screen added

---
+++ js/splash.js
@@ -0,0 +1,7 @@
+const splash = document.querySelector('.splash-style')
+
+document.addEventListener('DOMContentLoaded', (e) => {
+    setTimeout(() =>{
+        splash.classList.add('display-none');
+    }, 5000)
+} )
